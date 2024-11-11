6ed2cf4644e8cbb3d31082d5a254dd818f996ad2
Fatiha <163315691+FatihaSaidani@users.noreply.github.com>
2024-11-11 14:59:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from character import Enemy
 from character import Friendly
 from item import Keys
 
+# setting the rooms
 kitchen = Room("Kitchen")
 ballroom = Room("Ballroom")
 dining_hall = Room("Dining hall")
 
+# setting the character details and location
 dave = Enemy("Dave", "a smelly zombie", "cheese")
 dave.set_conversation("Hi I'm Dave, totally wont eat your brains!")
 dave.set_weakness("cheese")
 dining_hall.set_character(dave)
 
+# setting the character details and location
 cat = Friendly("cat", "A cute fluffy pet that is immune from zombies", "cuddles")
 cat.set_weakness("cuddles")
 kitchen.set_character(cat) 
 
+# setting the item details 
 key = Keys("key", "This item can help you unlock doors, if you are willing to take the risk")
 kitchen.set_item(key)
 
@@ -23,6 +27,7 @@ kitchen.set_description("A dank and dirty room buzzing with flies")
 ballroom.set_description("A vast room with a shiny wooden floor")
 dining_hall.set_description("A large room with ornate golden decorations")
 
+# allowing the rooms to be linked to eachother/next to eachother
 kitchen.link_room(dining_hall, "south")
 dining_hall.link_room(kitchen, "north")
 dining_hall.link_room(ballroom, "west")
@@ -30,15 +35,19 @@ ballroom.link_room(dining_hall, "east")
 
 ballroom.lock_room()
 
+# starting point for user
 current_room = kitchen
 player_inventory = []
 
+
+# Main while loop
 while True:
     print("\n")
     current_room.get_details()
     inhabitant = current_room.get_character()
     room_item = current_room.get_item()
 
+    # if statement that allows the user to interact
     if room_item is not None:
         print("--------------------------------------------")
         print(f"You have found a {room_item.get_name()} here.")
@@ -52,7 +61,7 @@ while True:
     if inhabitant is not None:
         inhabitant.describe()
 
-    # Main loop for game
+    # Main loop for game, giving the user options
     command = input("Would you like to talk, fight, or move to another room? > ").lower()
 
     if command == "talk":
@@ -62,6 +71,7 @@ while True:
         else:
             print("There's no one to talk to.")
 
+    # allows the user to fight the characters
     elif command == "fight":
         if inhabitant is not None:
             print("Choose a weapon to fight with > ")
@@ -73,11 +83,13 @@ while True:
         else:
             print("There's no one to fight.")
 
+    # allows the user to move around to different rooms
     elif command in ["north", "south", "east", "west"]:
         next_room = current_room.move(command)
         if next_room is not None:
             current_room = next_room
 
+    # allows the user to utilise the key if collected
     elif command == "unlock":
         direction = input("Which door would you like to unlock (north/south/east/west)? > ").lower()
         if direction in current_room.linked_rooms:
